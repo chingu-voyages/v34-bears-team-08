925eaffe9ea7906906538e191ccc5b2db8503dc9
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-10-09 16:00:42 -0700

    folder structure components -> lib/components

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,27 @@
-import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-netlify';
-import path from 'path';
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-netlify'
+import path from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        vite: {
-            resolve:{
-                alias: {
-                    $components:path.resolve("src/components/")
-                }
-            }
+  kit: {
+    vite: {
+      resolve: {
+        alias: {
+          $components: path.resolve('src/lib/components/'),
         },
-        adapter:adapter(),
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	},
+      },
+    },
+    adapter: adapter(),
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+  },
 
-    preprocess: [preprocess({
-        postcss: true
-    })]
-};
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
+}
 
-export default config;
+export default config
