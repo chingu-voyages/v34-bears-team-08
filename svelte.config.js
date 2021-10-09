599ac3f49fbf24d8d4e6091f94a8862bed36be95
rkr-dev <object.prototype.js@gmail.com~>
2021-10-08 21:18:23 -0700

    Netlify Cofigured
     - Netlify Configured

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
+        adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	},
