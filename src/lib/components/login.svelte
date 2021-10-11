8feae7e2318ec4555658970e85743af77088a3d9
Keith Frazier <keithers98@gmail.com>
2021-10-10 17:38:31 -0700

    built login component

--- src/lib/components/login.svelte
+++ src/lib/components/login.svelte
@@ -1,14 +1,76 @@
-<!-- <style>
-    button{
-        background-color: black;
-        color: #fff;
-        transition: all 300 ease-in-out;
-    }
-    button:hover{
-        transform: scale(1.1);
-    }
-</style> -->
+<script>
+  import GitHubIcon from '@svicons/fa-brands/github.svelte'
+  import GitLabIcon from '@svicons/fa-brands/gitlab.svelte'
+  import BitBucketIcon from '@svicons/fa-brands/bitbucket.svelte'
+  let error = false
+  let newUser = true
 
-<button class="bg-black text-white p-3 rounded-md hover:scale-100">
-    Login with Github
-</button>
+  let loginOrSignUp = { type: 'Sign up', function: signUp }
+
+  function changeLoginOrSignUp() {
+    if (newUser) {
+      loginOrSignUp.type = 'Sign up'
+      loginOrSignUp.function = signUp
+    } else {
+      loginOrSignUp.type = 'Login'
+      loginOrSignUp.function = login
+      loginOrSignUp.signUpBtn = ''
+    }
+  }
+
+  $: newUser = changeLoginOrSignUp()
+
+  function signUp() {
+    console.log('sign in')
+  }
+
+  function login() {
+    console.log('login')
+  }
+</script>
+
+<div class="container flex mx-auto max-w-screen-md items-center h-screen">
+  <div class="flex w-3/5">
+    <!--Add Image with mobile preview-->
+    <img src="/images/iphone.png" alt="iPhone with app on screen" />
+  </div>
+  <div class="flex flex-col w-3/5">
+    <div class="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+      <h1 class="flex justify-center w-full">
+        <img src="/images/devvy-01.png" alt="Placeholder" class="mt-2 w-3/4 mb-4" />
+      </h1>
+
+      {#if error} <p class="mb-4 text-xs text-red-primary">{error}</p> {/if}
+    </div>
+
+    <div class="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+      <button
+        class="bg-black flex flex-row text-black p-3 rounded-md hover:scale-100"
+        on:click={loginOrSignUp.function}
+      >
+        {loginOrSignUp.type} with GitHub <GitHubIcon class="pl-4 text-3xl" width="1.5em" color="black" />
+      </button>
+      <button
+        class="bg-black flex flex-row text-black p-3 rounded-md hover:scale-100"
+        on:click={loginOrSignUp.function}
+      >
+        {loginOrSignUp.type} with BitBucket <BitBucketIcon class="pl-4 text-3xl" width="1.5em" color="black" />
+      </button>
+      <button
+        class="bg-black flex flex-row text-black p-3 rounded-md hover:scale-100"
+        on:click={loginOrSignUp.function}
+      >
+        {loginOrSignUp.type} with GitLab <GitLabIcon class="pl-4 text-3xl" width="1.5em" color="black" />
+      </button>
+    </div>
+
+    {#if newUser}
+      <div class="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
+        <p class="text-sm">
+          Don't have an account?{` `}
+          <a href="/login" class="font-bold text-blue-medium"> Sign up </a>
+        </p>
+      </div>
+    {/if}
+  </div>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,27 @@ const config = {
   purge: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {}
+    fill: (theme) => ({
+      red: theme('colors.red.primary')
+    }),
+    colors: {
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#262626',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
+      }
+    }
   },
 
   plugins: [],
