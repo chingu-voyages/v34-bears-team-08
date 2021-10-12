169dab9be2d0b88c20458845fa6115c0578247f9
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-10-12 03:54:39 -0700

    add placeholders for SSR in homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,15 @@
-<script>
-      import Header from '$components/header.svelte'
-      import Loader from '$lib/components/loader.svelte';
+<script context="module">
+  export function load({ session, fetch }) {
+    // we can use this for SSR
+    return {}
+  }
 </script>
+
+<script>
+  import Header from '$components/header.svelte'
+  import Loader from '$lib/components/loader.svelte'
+</script>
+
 <svelte:head>
-    <title>Home</title>
+  <title>Home</title>
 </svelte:head>
