bc85f4b29f52f41b3f88d23b808c7de18eeeeba9
Keith Frazier <keithers98@gmail.com>
2021-10-11 19:08:35 -0700

    created loader animation component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
       import Header from '$components/header.svelte'
+      import Loader from '$lib/components/loader.svelte';
 </script>
 <svelte:head>
     <title>Home</title>
 </svelte:head>
-<Header/>
