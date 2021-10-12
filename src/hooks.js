ffac5f269aa556d04003bb598e271bac14e9b3c0
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-10-12 03:52:39 -0700

    add decryption of cookies to hooks

--- src/hooks.js
+++ src/hooks.js
@@ -1,15 +1,11 @@
-// export async function getSession(req, resolve) {
-//   // getSession runs server side so we're safe to parse cookies with our env encryption secret.
-//   // const cookies = cookie.parse(request.headers.cookie || '')
-//   // const user = await getSession(cookies['session'])
-//   // request.locals.user = user;
-//   // request.locals.userid = user?.publicAddress;
-//   // TODO: Decrypt cookies. Setting cookies is done by api/login endpoint.
-//   // TODO: Need to know if session is also readable from the server
-//   // The output of session will be available on both server and client load funcs.
-//   // return {
-//   //   userInfo,
-//   //   exp,
-//   //   token,
-//   // }
-// }
+import { decrypt } from './routes/api/auth/_utils'
+import cookie from 'cookie'
+
+export async function getSession(req) {
+  // getSession runs server side so we're safe to parse cookies with our env encryption secret.
+  const cookies = cookie.parse(req.headers.cookie || '')
+  const authData = await decrypt(cookies.session)
+
+  // The output of session will be available on both server and client load funcs.
+  return authData || {} // creating this cookie is done by auth/login endpoint
+}
