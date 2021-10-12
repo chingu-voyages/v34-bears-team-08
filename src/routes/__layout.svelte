3994542d5337c30e3f9be978e62d81d8cdafec27
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-10-12 03:54:19 -0700

    set the decrypted auth token every time from layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,2 +1,14 @@
-<script>import "../app.css";</script>
-<slot></slot>
+<script context="module">
+  import { auth } from '$lib/stores/auth'
+  export function load({ session }) {
+    // we have an auth token, which means we have a session
+    if (session?.token) auth.set(session)
+    return {}
+  }
+</script>
+
+<script>
+  import '../app.css'
+</script>
+
+<slot />
