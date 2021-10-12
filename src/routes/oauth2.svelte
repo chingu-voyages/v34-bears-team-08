6c87685c02f4ff51719f300481541b9dd658697a
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-10-11 22:10:39 -0700

    oauth2 callback route

---
+++ src/routes/oauth2.svelte
@@ -0,0 +1,17 @@
+<!-- OAuth2 callback route -->
+<script>
+  import { goto } from '$app/navigation'
+  import { magic } from '$lib/utils/magic'
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    magic.oauth.getRedirectResult().then(
+      (result) => {
+        // TODO: ID Token is critical. Use it for logging in!
+        // result.magic = { userMetadata, idToken }
+        // result.oauth.userInfo = { email, emailVerified: boolean, picture, profile }
+      },
+      () => goto('/login') // error
+    )
+  })
+</script>
