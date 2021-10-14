d8ee8e84cab7992618b5e0bc73f0006af63713da
Keith Frazier <keithers98@gmail.com>
2021-10-14 14:50:11 -0700

    added base explore layout

---
+++ src/routes/explore.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Header from '$lib/components/header.svelte'
+</script>
+
+<Header />
+<div class="w-full flex flex-row justify-center">
+  <div class="flex flex-wrap justify-between w-4/5">
+    <div class="placeholder mt-10" />
+    <div class="placeholder mt-10" />
+    <div class="placeholder mt-10" />
+    <div class="placeholder mt-10" />
+    <div class="placeholder mt-10" />
+    <div class="placeholder mt-10" />
+    <div class="placeholder mt-10" />
+    <div class="placeholder mt-10" />
+    <div class="placeholder mt-10" />
+    <div class="placeholder mt-10" />
+    <div class="placeholder mt-10" />
+    <div class="placeholder mt-10" />
+  </div>
+</div>
+
+<style>
+  .placeholder {
+    width: 300px;
+    height: 300px;
+    background-color: black;
+  }
+</style>
