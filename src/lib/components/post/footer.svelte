0fc994ec74baa6433cdde36cf587ecd69c0cc2ee
rkr-dev <object.prototype.js@gmail.com>
2021-10-15 18:39:20 -0700

    Moved post to src/lib/components
    - Deleted src/lib/components/post
    - Moved src/lib/post to src/lib/components

--- src/lib/components/post/footer.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  export let username;
-  export let caption;
-</script>
-
-<div class="p-4 pt-2 pb-1">
-    <span class="mr-1 font-bold">{username}</span>
-    <span class="italic">{caption}</span>
-  </div>
--- src/lib/components/post/image.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-export let src;
-export let caption;
-
-</script>
-    
-<img src={src} alt={caption} />;
