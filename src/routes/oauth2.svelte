66e4c49f30a30adc9f15204a03a0f8178f40427b
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-10-15 20:14:55 -0700

    add loader

--- src/routes/oauth2.svelte
+++ src/routes/oauth2.svelte
@@ -1,10 +1,12 @@
 <!-- OAuth2 callback route -->
 <script>
   import { goto } from '$app/navigation'
+  import Loader from '$lib/components/Loader.svelte'
   import { verify } from '$lib/stores/auth'
   import { magic } from '$lib/utils/magic'
   import { onMount } from 'svelte'
 
+  let loading = true
   onMount(async () => {
     /* res = {
       magic: { userMetadata, idToken }
@@ -15,7 +17,14 @@
     const res = await magic.oauth.getRedirectResult()
     await verify({ email: res.magic?.userMetadata.email, idToken: res.magic?.idToken })
     // TODO: If this isn't causing a redirect that reloads __layout, then we need to just use the native location API.
+    loading = false
     goto('/', { replaceState: true })
     // ? Deal with possible errors? Try doing this on error? goto('/login')
   })
 </script>
+
+{#if loading}
+  <main class="grid place-items-center min-h-screen">
+    <Loader class="opacity-75" />
+  </main>
+{/if}
