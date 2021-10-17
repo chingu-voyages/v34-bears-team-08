c806047f91bf9fa4f2120a29401886e0a4088a85
Keith Frazier <keithers98@gmail.com>
2021-10-17 16:16:04 -0700

    changed import from header.svelte to Header.svelte in)layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,7 +12,7 @@
 
 import '../app.css'
 import Footer from '$lib/components/Footer.svelte';
-import Header from '$lib/components/header.svelte';
+import Header from '$lib/components/Header.svelte';
 
   initClient()
 
