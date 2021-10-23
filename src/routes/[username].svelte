6486eafb164cc70e7cd7c445a2ac82f257bbf429
Keith Frazier <keithers98@gmail.com>
2021-10-23 13:30:18 -0700

    added delete button and query

--- src/routes/[username].svelte
+++ src/routes/[username].svelte
@@ -7,22 +7,50 @@ export function load({ page }) {
 
 <script>
 import ProfileInfo from '$lib/components/ProfileInfo.svelte'
-import { GetCurrentUserPhotos } from '$lib/gql/GetProfilePhotos';
+import { GetCurrentUserPhotos } from '$lib/gql/GetProfilePhotos'
+import { mutationOp } from '$lib/gql/urql'
+import Times from '@svicons/fa-solid/times.svelte'
+import { gql } from '@urql/core'
 export let username
 username = username.slice(1)
 
-console.dir(GetCurrentUserPhotos({username}))
+GetCurrentUserPhotos({ username })
 $: photoArr = $GetCurrentUserPhotos.data?.result.data || []
-$: console.log('photos', photoArr)
+
+const deletePhoto = mutationOp(gql`
+  mutation deletePhoto($id: ID!) {
+    result: deletePhoto(id: $id) {
+      _id
+    }
+  }
+`)
+
+function deletePost(e) {
+  let id = e.target.dataset.photoId
+  if (confirm('Are you sure you want to delete this post?')) {
+    deletePhoto({id})
+  }
+}
 </script>
 
 <div class="flex flex-col items-center h-screen mt-10">
   <div class="flex flex-row justify-between w-3/5">
     <ProfileInfo {username} />
   </div>
-  <ul class="flex flex-wrap justify-between w-3/5">
+  <ul class="flex flex-wrap justify-between w-3/5 relative">
     {#each photoArr as photo, index}
-      <li><img src={photo.src} width="300px" alt="photo #{index + 1}" class="mt-5" /></li>
+      <li class="w-max relative">
+        <div class="w-full">
+          <img src={photo.src} width="300px" alt="photo #{index + 1}" class="mt-5 z-0" />
+          <button
+            class="z-10 display-block absolute top-0 right-0 mt-7 ml-3 text-white text-lg pr-4 pb-4"
+            on:click={deletePost}
+            data-photo-id={photo._id}
+          >
+            x
+          </button>
+        </div>
+      </li>
     {/each}
   </ul>
 </div>
