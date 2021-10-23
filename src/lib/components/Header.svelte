cef63e7191bf3ef223bb759f5dce39c64e1dd7fd
rkr-dev <object.prototype.js@gmail.com>
2021-10-22 19:56:39 -0700

    Create Post
     - Modal Updated
     - Removed Post stores
     - Header updated

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,5 @@
 <script>
   import { auth, isAuthenticated, logout } from '$lib/stores/auth';
-  //import {filesForUpload, upload} from '$lib/stores/post'
   import Modal from '$components/Modal.svelte';
   import {Home, Compass, SignOutAlt, UserAlt, PlusCircle} from '@svicons/fa-solid'
   import Logo from '$components/Logo.svelte'
@@ -9,7 +8,6 @@
   disableLogout = false
   console.log($isAuthenticated)
   let showModal = false;
-  let file; 
 </script>
 
 <header class="px-4 h-16 bg-white border-b border-gray-primary mb-8">
@@ -90,17 +88,9 @@
     </div>
   </div>
 </header>
-<!--
+
+
 {#if showModal}
-<Modal on:close="{() => showModal = false}">
-	<h2 slot="header" class="w-full text-center text-4xl font-bold px-2 mb-2">
-			Create a post
-	</h2>
-	<form class="w-full flex flex-col content-center items-center">
-		<input type="file" class="w-full my-5" bind:files={$filesForUpload}/>
-    <textarea  class="w-full focus:outline-none" name="" id="" cols="3" rows="2" placeholder="Write a caption"></textarea>
-    <button on:submit|preventDefault= {upload}">post</button>
-  </form>
-</Modal>
-{/if}-->
+<Modal on:close="{() => showModal = false}"   />
+{/if}
 
