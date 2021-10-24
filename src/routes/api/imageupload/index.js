a97e360cd1c988ada978066ce6e937cab1ff840d
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-10-23 18:06:11 -0700

    fix & refactor api/imageupload

--- src/routes/api/imageupload/index.js
+++ src/routes/api/imageupload/index.js
@@ -1,15 +1,16 @@
-import ImageKit from 'imagekit';
+import ImageKit from 'imagekit'
 import { v4 as uuidv4 } from 'uuid'
 
 const imagekit = new ImageKit({
-  publicKey: process.env['ImageKit_PUBLIC_KEY'],
-  privateKey: process.env['ImageKit_PRIVATE_KEY'],
-  urlEndpoint: process.env['ImageKit_URL_END_POINT'],
+  publicKey: import.meta.env.VITE_IMAGEKIT_PUBLIC_KEY,
+  privateKey: import.meta.env.PROD ? process.env['IMAGEKIT_PRIVATE_KEY'] : import.meta.env.VITE_IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: import.meta.env.VITE_IMAGEKIT_URL_END_POINT,
 })
 
-export async function post() {
-  const token = uuidv4() // req.query.token
-  const expiration = parseInt(Date.now() / 1000) + 60 * 10 // or req.query.expiration
-  const signatureObj = await imagekit.getAuthenticationParameters()
-  return { status: 200, signatureObj }
+export async function get({ query }) {
+  const token = query.token || uuidv4()
+  const expiration = query.expiration || parseInt(Date.now() / 1000) + 60 * 10
+  const signatureObj = await imagekit.getAuthenticationParameters(token, expiration)
+  console.log(signatureObj)
+  return { status: 200, body: signatureObj }
 }
