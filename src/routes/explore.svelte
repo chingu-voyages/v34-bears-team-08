fbb436279cf1a08e2698f75499578d1d22213db7
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-11-01 09:00:37 -0700

    link to profile

--- src/routes/explore.svelte
+++ src/routes/explore.svelte
@@ -26,7 +26,9 @@ $: photoArr = $Timeline.data?.result.data || []
         <img src={photo.src} width="400px" alt="{photo.author.username}'s photo" />
         <div class="opacity-0 hover:opacity-100 w-full h-full z-10 display-block absolute top-0 left-0 text-white">
           <div class="w-full h-full display-flex absolute top-0 left-0 bg-black-light opacity-40 z-20" />
-          <button class="z-30 display-block absolute top-0 left-0 m-2">{photo.author.username}</button>
+          <a class="z-30 display-block absolute top-0 left-0 m-2" href="/{photo.author.username}" sveltekit:prefetch
+            >{photo.author.username}</a
+          >
           <div class="flex w-full h-full flex-row justify-center items-center">
             <span class="z-30 mr-5">{photo.likeCount || 0} likes</span>
             <span class="z-30">{photo.comments.data.length || 0} comments</span>
