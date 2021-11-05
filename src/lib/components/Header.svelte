6c0030ff629262f96da48098d1eae1a5b02edb20
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-11-04 18:26:03 -0700

    button for mobile dimensions

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -24,12 +24,12 @@ $: if (!quickOpen) searchMode = false
       <div class="text-gray-700 text-center flex items-center align-items space-x-5">
         {#if $isAuthenticated}
           <button
-            class="rounded-full text-gray-gray11 bg-whiteA-whiteA5 p-2 hover:text-whiteA-whiteA11 hover:bg-whiteA-whiteA6"
+            class="fixed bottom-6 right-6 p-4 sm:static rounded-full text-gray-gray11 bg-whiteA-whiteA5 sm:p-2 hover:text-whiteA-whiteA11 hover:bg-whiteA-whiteA6"
             aria-label="quick actions"
             on:click={() => (quickOpen = !quickOpen)}
           >
             <LightningCharge
-              class="w-6"
+              class="w-8 sm:w-6"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -38,28 +38,23 @@ $: if (!quickOpen) searchMode = false
           </button>
 
           <!--This conditional will be replaced with some user data containing a user image (avatar)-->
-          {#if $isAuthenticated}
-            <a sveltekit:prefetch class="rounded-full bg-whiteA-whiteA5 p-2 hover:bg-whiteA-whiteA6" href="/{username}">
-              <!-- TODO remove this line when profile photo upload func implemented -->
-              <img class="h-6 w-6 rounded-full" src="https://picsum.photos/24" alt="" />
-              {#if $auth.userInfo?.profileImgSrc}
-                <img
-                  class="inline-block h-6 w-6 rounded-full"
-                  src={$auth.userInfo?.profileImgSrc}
-                  alt="profile avatar"
-                />
-                <!-- {:else} -->
-                <!-- placeholder -->
-                <span class="inline-block h-6 w-6 rounded-full overflow-hidden bg-gray-300">
-                  <svg class="h-full w-full text-gray-800" fill="currentColor" viewBox="0 0 24 24">
-                    <path
-                      d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"
-                    />
-                  </svg>
-                </span>
-              {/if}
-            </a>
-          {/if}
+
+          <a sveltekit:prefetch class="rounded-full bg-whiteA-whiteA5 p-2 hover:bg-whiteA-whiteA6" href="/{username}">
+            <!-- TODO remove this line when profile photo upload func implemented -->
+            <img class="h-6 w-6 rounded-full" src="https://picsum.photos/24" alt="" />
+            {#if $auth.userInfo?.profileImgSrc}
+              <img class="inline-block h-6 w-6 rounded-full" src={$auth.userInfo?.profileImgSrc} alt="profile avatar" />
+              <!-- {:else} -->
+              <!-- placeholder -->
+              <span class="inline-block h-6 w-6 rounded-full overflow-hidden bg-gray-300">
+                <svg class="h-full w-full text-gray-800" fill="currentColor" viewBox="0 0 24 24">
+                  <path
+                    d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                </svg>
+              </span>
+            {/if}
+          </a>
         {/if}
       </div>
     </div>
