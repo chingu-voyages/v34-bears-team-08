7cf516e34923df690e870bd59da597763f10d182
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-11-21 20:31:33 -0800

    keep it DRY

--- src/lib/components/ProfileInfo.svelte
+++ src/lib/components/ProfileInfo.svelte
@@ -36,40 +36,33 @@ let settingsOpen = false,
   <div class="flex flex-col w-3/4">
     <div class="flex flex-row justify-between place-items-center">
       <h2 class="my-1 text-[2.25rem] overflow-ellipsis overflow-hidden">{fullName || username}</h2>
-      <button
-        class="w-8 sm:w-6 text-gray-gray11 hover:text-white"
-        on:click={() => {
-          settingsOpen = true
-        }}><Gear /></button
+      <button class="w-8 sm:w-6 text-gray-gray11 hover:text-white" on:click={() => (settingsOpen = true)}
+        ><Gear /></button
       >
     </div>
-    <div class="flex items-center ">
+    <div class="flex items-center">
       <button
-        class="text-sm inline-block mr-2"
+        class="text-sm inline-block mr-2 disabled:cursor-default"
+        disabled={!followingCount}
         on:click={() => {
           followers = false
           followsOpen = true
         }}>{followingCount || 0} following</button
       >
-      <button class="text-sm inline-block" on:click={() => (followers = followsOpen = true)}
-        >{followerCount || 0} followers</button
+      <button
+        class="text-sm inline-block disabled:cursor-default"
+        disabled={!followerCount}
+        on:click={() => (followers = followsOpen = true)}>{followerCount || 0} followers</button
       >
     </div>
     {#if headline}<h3 class="my-1">{headline}</h3>{/if}
     <p class="my-1">{bio}</p>
-    {#if currentUser != username && followedByUser}
+    {#if currentUser != username}
       <button
         class="bg-blue-400 rounded-md py-1 px-2 w-min text-white"
-        on:click={() => execFollowUser({ id: _id, value: false, user: $UserInfo.data?.result })}
+        on:click={() => execFollowUser({ id: _id, value: !followedByUser, user: $UserInfo.data?.result })}
       >
-        Unfollow
-      </button>
-    {:else if currentUser != username}
-      <button
-        class="bg-blue-400 rounded-md py-1 px-2 w-min text-white"
-        on:click={() => execFollowUser({ id: _id, user: $UserInfo.data?.result })}
-      >
-        Follow
+        {followedByUser ? 'Unfollow' : 'Follow'}
       </button>
     {/if}
   </div>
